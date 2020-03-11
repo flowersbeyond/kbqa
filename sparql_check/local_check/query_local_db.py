9375cf78b40457a41292bba00eaefa6384430939
Xinrui Guo <xingu@microsoft.com>
2020-03-11 17:28:17 +0800

    AddFeature: Add interactive query interface.

--- sparql_check/local_check/query_local_db.py
+++ sparql_check/local_check/query_local_db.py
@@ -1,31 +1,53 @@
 from rdflib.graph import Graph, ConjunctiveGraph
 from rdflib.plugins.memory import IOMemory
 
+import multiprocessing as mp
+
+
+if __name__ == '__main__':
+    mp.set_start_method('spawn')
+    q = mp.Queue()
+    p = mp.Process(target=foo, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
+
+def load_core_data(graph, file, name):
+    print(name + '\t loading...')
+    graph.parse(file, format='turtle')
+    print(name + '\tfinished loading.')
 
 def load_all_core_data(data_dir, core_names):
     store = IOMemory()
 
     g = ConjunctiveGraph(store=store)
-
+    p_list = []
     for name in core_names:
         gi = Graph(store=store, identifier=name)
-        print (name + '\t loading...')
-        gi.parse('%s/%s.ttl'%(data_dir, name), format='turtle')
-        print (name + '\tfinished loading.')
+
+        p = mp.Process(target=load_core_data, args=(gi, '%s/%s.ttl'%(data_dir, name), name))
+        p_list.append(p)
+        p.start()
+
+    for p in p_list:
+        p.join()
 
     # enumerate contexts
     for c in g.contexts():
         print("-- %s " % c)
 
 
+    print('begin sample query:')
     qres = g.query(
         """sparql" : "PREFIX res: <http://dbpedia.org/resource/> PREFIX dbp: <http://dbpedia.org/property/> 
         SELECT DISTINCT ?uri WHERE { res:Salt_Lake_City <http://dbpedia.org/ontology/timeZone> ?uri }"""
     )
 
+    print('end sample query')
     for row in qres:
         print(row)
 
+    return g
 
 
 if __name__ == '__main__':
@@ -41,4 +63,15 @@ if __name__ == '__main__':
         'persondata_en'
     ]
 
-    load_all_core_data(data_dir, core_names)
+    g = load_all_core_data(data_dir, core_names)
+
+    while(True):
+        query = input('query:')
+
+        if query.startswith('Q:'):
+            query = query[2:]
+            print('begin query...')
+            qres = g.query(query)
+            for row in qres:
+                print(row)
+            print('end query...')
