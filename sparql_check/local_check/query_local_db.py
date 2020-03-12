9fad0c8f1a4873c9ed8b67acedee23f8bbdd8688
Xinrui Guo <xingu@microsoft.com>
2020-03-12 19:03:48 +0800

    AddFeature: Able to run local db check with input/output in files

--- sparql_check/local_check/query_local_db.py
+++ sparql_check/local_check/query_local_db.py
@@ -1,68 +1,106 @@
-from rdflib.graph import Graph, ConjunctiveGraph
-from rdflib.plugins.memory import IOMemory
+from rdflib.graph import Graph
+import json
 
-import multiprocessing as mp
-
-
-def load_core_data(graph, file, name):
-    print(name + '\t loading...')
-    graph.parse(file, format='turtle')
-    print(name + '\tfinished loading.')
 
 def load_all_core_data(data_dir, core_names):
-    store = IOMemory()
 
-    g = Graph()#store=store)
-    p_list = []
+    g = Graph()
+
     for name in core_names:
-        #gi = Graph(store=store, identifier=name)
-
-        p = mp.Process(target=load_core_data, args=(g, '%s/%s.ttl'%(data_dir, name), name))
-        p_list.append(p)
-        p.start()
-
-    for p in p_list:
-        p.join()
-
-    # enumerate contexts
-    #for c in g.contexts():
-    #    print("-- %s " % c)
-
-
-    try:
-        print('begin sample query:')
-        qres = g.query(
-            """PREFIX res: <http://dbpedia.org/resource/> PREFIX dbp: <http://dbpedia.org/property/> 
-            SELECT DISTINCT ?uri WHERE { res:Salt_Lake_City <http://dbpedia.org/ontology/timeZone> ?uri }"""
-        )
-
-        print('end sample query')
-        for row in qres:
-            print(row)
-    except Exception as e:
-        print (str(e))
+        print(name + '\t loading...')
+        g.parse('%s/%s.ttl'%(data_dir, name), format='turtle')
+        print(name + '\tfinished loading.')
 
     return g
 
 
+def interpret_result(qres):
+    answers = []
+
+    if qres.bindings != None:
+
+        for binding in qres.bindings:
+            for var in qres.vars:
+                answer = {str(var): str(binding[var])}
+                answers.append(answer)
+
+    elif qres.askAnswer != None:
+        answers = [{'bool': str(qres.askAnswer)}]
+
+    else:
+        for row in qres:
+            answers.append((str(row)))
+
+    return answers
+
+
+def extract_id_query(file):
+
+    questions = {}
+    with open(file, encoding='utf-8') as fin:
+        for l in fin:
+            question = json.loads(l)
+            id = question['id']
+            query = question['query']['sparql']
+            questions[id] = query
+    return questions
+
+
 if __name__ == '__main__':
-    data_dir = './data/DBPedia/core8'
+
+    qald_multilingual_train = './data/QALD/train-multilingual-4-9.jsonl'
+    qald_multilingual_test = './data/QALD/test-multilingual-4-9.jsonl'
+
+    train_result = './data/QALD/train-multilingual-4-9_local_result.jsonl'
+    test_result = './data/QALD/test-multilingual-4-9_local_result.jsonl'
+
+    train_queries = extract_id_query(qald_multilingual_train)
+    test_queries = extract_id_query(qald_multilingual_test)
+
+
+    dbpedia_data_dir = './data/DBPedia/core8/'
     core_names = [
-        #'labels_en',
+        'labels_en',
         'category_labels_en',
 
-        #'article_categories_en',
-        #'instance_types_en',
-        #'infobox_properties_en',
-        #'mappingbased_literals_en',
-        #'mappingbased_objects_en',
-        #'persondata_en'
+        'article_categories_en',
+        'instance_types_en',
+        'infobox_properties_en',
+        'mappingbased_literals_en',
+        'mappingbased_objects_en',
+        'persondata_en'
 
     ]
 
-    g = load_all_core_data(data_dir, core_names)
+    g = load_all_core_data(dbpedia_data_dir, core_names)
 
-    while(True):
+    with open(train_result, encoding='utf-8', mode='w') as fout:
+
+        for id in train_queries:
+            try:
+                qres = g.query(train_queries[id])
+                answers = interpret_result(qres)
+                fout.write(json.dumps({'id':id, 'answer':answers}))
+                fout.write('\n')
+
+            except Exception as e:
+                fout.write(json.dumps({'id':id, 'error':str(e)}))
+                fout.write('\n')
+
+    with open(test_result, encoding='utf-8', mode='w') as fout:
+
+        for id in test_queries:
+            try:
+                qres = g.query(test_queries[id])
+                answers = interpret_result(qres)
+                fout.write(json.dumps({'id':id, 'answer':answers}))
+                fout.write('\n')
+
+            except Exception as e:
+                fout.write(json.dumps({'id':id, 'error':str(e)}))
+                fout.write('\n')
+
+    while (True):
         query = input('query:')
 
         if query.startswith('Q:'):
@@ -70,12 +108,17 @@ if __name__ == '__main__':
             print('begin query...')
             try:
                 qres = g.query(query)
-                for row in qres:
-                    print(row)
+                if qres.bindings != None:
+                    for binding in qres.bindings:
+                        for var in qres.vars:
+                            print('%s:\t%s\n' % (str(var), str(binding[var])))
+
+                elif qres.askAnswer != None:
+                    print(qres.askAnswer)
+
             except Exception as e:
                 print(str(e))
 
             print('end query...')
         elif query == 'exit':
             break
-
